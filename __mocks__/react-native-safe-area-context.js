f9320262076a323bb85d931829c86deb29c66166
isabellenelson <isabellenoelnelson@gmail.com>
2026-04-10 15:39:45 -0400

    fix: use React.Fragment in SafeAreaView mock to avoid native bridge dependency

--- __mocks__/react-native-safe-area-context.js
+++ __mocks__/react-native-safe-area-context.js
@@ -1,8 +1,7 @@
 const React = require('react');
-const { View } = require('react-native');
 
-const SafeAreaView = ({ children, style }) => React.createElement(View, { style }, children);
-const SafeAreaProvider = ({ children }) => React.createElement(View, null, children);
+const SafeAreaView = ({ children, style }) => React.createElement(React.Fragment, null, children);
+const SafeAreaProvider = ({ children }) => React.createElement(React.Fragment, null, children);
 const useSafeAreaInsets = () => ({ top: 0, right: 0, bottom: 0, left: 0 });
 const useSafeAreaFrame = () => ({ x: 0, y: 0, width: 375, height: 812 });
 const initialWindowMetrics = {
